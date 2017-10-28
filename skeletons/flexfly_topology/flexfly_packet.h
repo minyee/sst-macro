e4d77f4a3eb488c150cf395b94216f261cac0828
minyee <mt3126@columbia.edu>
2017-10-28 02:22:55 -0400

    minor changes to flexfly_packet.h

--- skeletons/flexfly_topology/flexfly_packet.h
+++ skeletons/flexfly_topology/flexfly_packet.h
@@ -1,5 +1,6 @@
 #include <routable.h>
-#include <sstmac/hw/common/packet.h>
+#include <sstmac/hardware/pisces/pisces.h>
+
 namespace sstmac{
 namespace hw {
 
@@ -12,17 +13,48 @@ struct switch_port_pair {
 };
 
 struct flexfly_path {
-	std::queue< switch_port_pair >;
+	std::queue<switch_port_pair>;
 };
 
-class flexfly_packet : public packet {
+class flexfly_packet : public pisces_payload {
+
+ImplementSerializable(flexfly_packet);
+
 public:
 	flexfly_packet();
+	
 	~flexfly_packet() {
+		if (path_)
+			delete path_;
+	}; 
 
+	flexfly_path* route_path() {
+		return path_;
+	};
+
+	/**
+	 * pops the pointer to the next switch port pair. returns a nullptr if there are
+	 * no more switches in the path. Note that this will decrement the count of the 
+	 * switch__port_pair queue by 1.
+	 **/
+	switch_port_pair* next_switch() {
+		if (!path_)
+			return nullptr;
+		switch_port_pair* to_ret = path_->first();
+		path_->pop();
+		return to_ret;
 	}
+
+	void set_new_path(int dst_switch, int port_num) {
+		switch_port_pair* spp = new switch_port_pair;
+		spp->switch_id = dst_switch;
+		spp->port = port_num;
+		path_->push(spp);
+		return;
+	}
+
 private: 
-	path
+	flexfly_path *path_;
 };
 
 
