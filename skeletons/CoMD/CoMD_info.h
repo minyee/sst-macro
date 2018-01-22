033efd689e4282195707340cfb5bae7c9bb0de58
minyee <mt3126@columbia.edu>
2018-01-22 00:26:30 -0500

    time for paper writing and research

---
+++ skeletons/CoMD/CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "openmp-mpi"
+#define CoMD_HOSTNAME "dyn-160-39-52-174.dyn.columbia.edu"
+#define CoMD_KERNEL_NAME "'Darwin'"
+#define CoMD_KERNEL_RELEASE "'17.2.0'"
+#define CoMD_PROCESSOR "'i386'"
+
+#define CoMD_COMPILER "'/Users/minyee/install/sstmacro-7.1.0/bin/sstcc'"
+#define CoMD_COMPILER_VERSION "'Configured with: --prefix=/Library/Developer/CommandLineTools/usr --with-gxx-include-dir=/usr/include/c++/4.2.1'"
+#define CoMD_CFLAGS "'-std=c99 -fPIC -DDOUBLE -DDO_MPI -O3   '"
+#define CoMD_LDFLAGS "' -lm '"
+
+#endif
